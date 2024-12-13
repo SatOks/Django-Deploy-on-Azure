f7dd99593cc668ac09f9249e9b1cb7f0cf1568cf
SatOks <yurii.harhai.shi.2023@lpnu.ua>
2024-12-14 00:57:59 +0200

    once again new database settings

--- trello_clone/deployment.py
+++ trello_clone/deployment.py
@@ -32,21 +32,37 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 os.makedirs(BASE_DIR / 'static', exist_ok=True)
 os.makedirs(BASE_DIR / 'staticfiles', exist_ok=True)
 
-connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
-
-# deployment.py
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'trelloclone-database',
-        'HOST': 'trelloclone-server.postgres.database.azure.com',
-        'USER': 'jwesthypnb',
-        'PASSWORD': 'w9w9$ykq56nnkSlb',
-        'PORT': '5432',
-        'OPTIONS': {
-            'sslmode': 'require',
-            'connect_timeout': 30,
+# Parse connection string properly
+connection_string = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING', '')
+if connection_string:
+    parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': parameters['dbname'],
+            'HOST': parameters['host'],
+            'USER': parameters['user'],
+            'PASSWORD': parameters['password'],
+            'PORT': parameters.get('port', '5432'),
+            'OPTIONS': {
+                'sslmode': 'require',
+                'sslcert': None,
+            }
         }
     }
-}
+else:
+    # Fallback to direct configuration
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DBNAME', 'trelloclone-database'),
+            'HOST': os.environ.get('DBHOST', 'trelloclone-server.postgres.database.azure.com'),
+            'USER': os.environ.get('DBUSER', 'jwesthypnb'),
+            'PASSWORD': os.environ.get('DBPASS', 'w9w9$ykq56nnkSlb'),
+            'PORT': os.environ.get('DBPORT', '5432'),
+            'OPTIONS': {
+                'sslmode': 'require',
+                'sslcert': None,
+            }
+        }
+    }
